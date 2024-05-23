ac6de42a50fdc3c1a41e8ba7815c22ddaf9ee7ab
Bandi Narendra <narendranari7288@gmail.com>
2024-05-23 14:19:05 +0530

    initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Nxtwave
+This repo is to develop a Resource Management Admin portal. This portal consists of a list of resources. The admin should be able to see all the resources, see tag-wise resources, and add a new resource.
---
+++ src/App.js
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav class="navbar navbar-light bg-light">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              <img
+                src="nxtwaveIcon.svg"
+                alt=""
+                width="82"
+                height="40"
+                top="16"
+                left="48"
+                gap="0"
+                opacity="0"
+              />
+            </a>
+          </div>
+          <button type="button" class="btn btn-primary">
+            +ADD
+          </button>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
